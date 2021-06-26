4bd32c3b03885c110400c7e7f67ceffdc381244b
samaneh <samaneh.mazdarani1@gmail.com>
2021-06-26 12:39:42 -0700

    print

---
+++ count function.js
@@ -0,0 +1,12 @@
+function count (a, stirng){
+    let count =" ";
+    for ( let i=0 ;i<string.lenght;i++){
+        if string [i]=a;
+        count++;
+    }
+    return count;
+}
+console.log (count function ( a, banana));
+
+
+
